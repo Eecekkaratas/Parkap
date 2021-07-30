14541b092569d07811628c89a946a4669774b455
Onur <onursertgil23@gmail.com>
2021-07-30 11:01:23 +0300

    register fonksiyonu

--- lib/auth.dart
+++ lib/auth.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<bool> signIn(String email, String password) async {
   try {
@@ -27,4 +28,24 @@ Future<UserCredential> signInWithGoogle() async {
 
   // Once signed in, return the UserCredential
   return await FirebaseAuth.instance.signInWithCredential(credential);
+}
+
+Future<bool> register(String email, String password) async {
+  try {
+    await FirebaseAuth.instance
+        .createUserWithEmailAndPassword(email: email, password: password);
+    String uid = FirebaseAuth.instance.currentUser.uid;
+
+    return true;
+  } on FirebaseAuthException catch (e) {
+    if (e.code == 'weak-password') {
+      print('The password provided is too weak.');
+    } else if (e.code == 'email-already-in-use') {
+      print('The account already exists for that email.');
+    }
+    return false;
+  } catch (e) {
+    print(e.toString());
+    return false;
+  }
 }
